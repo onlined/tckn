1e36545a25aa74286487eee4fc8308249c63ebc9
Ekin Dursun <ekindursun@gmail.com>
2018-12-19 22:56:10 +0300

    Add last_2_digits

--- tckn.py
+++ tckn.py
@@ -7,11 +7,8 @@ def generate() -> str:
     digits = range(10)
     first_digit = random.randint(1, 9)  # Must be nonzero
     first_9_digits = [first_digit] + random.choices(digits, k=8)
-    digit_10 = (sum(first_9_digits[::2])*7 - sum(first_9_digits[1::2])) % 10
-    first_10_digits = first_9_digits + [digit_10]
-    digit_11 = sum(first_10_digits) % 10
-    all_digits = first_10_digits + [digit_11]
-    return ''.join(str(digit) for digit in all_digits)
+    first_part = ''.join(str(d) for d in first_9_digits)
+    return first_part + last_2_digits(first_part)
 
 
 def validate(number: str) -> bool:
@@ -28,3 +25,16 @@ def validate(number: str) -> bool:
     if digits[10] != sum(digits[:10]) % 10:
         return False
     return True
+
+
+def last_2_digits(number: str) -> str:
+    """Generate last 2 digits for first 9 digits of an identity number."""
+    if len(number) != 9:
+        raise ValueError('The number should have 9 digits')
+    if not all(d in string.digits for d in number):
+        raise ValueError('The characters must be all digits')
+    first_digits = [int(d) for d in number]
+    digit_10 = (sum(first_digits[::2])*7 - sum(first_digits[1::2])) % 10
+    first_10_digits = first_digits + [digit_10]
+    digit_11 = sum(first_10_digits) % 10
+    return str(digit_10) + str(digit_11)
